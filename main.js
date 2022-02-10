d08c643d7eb2d1319862dd784b04462f4ca1bc7b
Hitesh1436 <hsc253545@gmail.com>
2022-02-10 19:25:10 +0530

    added

--- main.js
+++ main.js
@@ -26,31 +26,5 @@ function extractLink(html){
     console.log(fullLink);
 
 
-    getAllMatchLink(fullLink);
+    allMatchObj.getAllMatch(fullLink);
 }
-
-function getAllMatchLink(uri){
-    request(uri , function(error,response,html){
-        if(error){
-            console.log(error);
-        }
-        else{
-            extractAllLink(html);
-        }
-    })
-}
-
-function extractAllLink(html){
-    let $ = cheerio.load(html);
-
-
-    let scoreCardArr = $('a[data-hover="Scorecard"]')
-
-    for(let i=0 ; i<scoreCardArr.length ;i++){
-        let link = $(scoreCardArr[i]).attr('href');
-        let fullLink = 'https://www.espncricinfo.com/' + link;
-        console.log(fullLink)
-    }
-}
-
-
